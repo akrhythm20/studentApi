b925f9fbd0f0596bab1576b5a364435a64a599ea
akrhythm20 <akrhythm20@gmail.com>
2021-06-27 14:41:24 +0530

    dbUpdate

--- curdapi/settings.py
+++ curdapi/settings.py
@@ -1,6 +1,4 @@
 from pathlib import Path
-import django_heroku
-import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -65,11 +63,11 @@ WSGI_APPLICATION = 'curdapi.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default' : dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+
 }
 
 
@@ -110,4 +108,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-django_heroku.settings(locals())

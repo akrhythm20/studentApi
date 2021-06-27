aece3f548c5827e0e97357f382ecb753e92ae560
akrhythm20 <akrhythm20@gmail.com>
2021-06-27 14:19:32 +0530

    db

--- curdapi/settings.py
+++ curdapi/settings.py
@@ -69,7 +69,7 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-    'default' : dj_database_url()
+    'default' : dj_database_url.config()
 }
 
 
